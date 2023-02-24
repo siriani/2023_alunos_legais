ed293e28f23d6a5943f1686889cf299cb4577063
Allan Siriani <allan@fatecpompeia.edu.br>
2023-02-24 17:01:18 -0300

    com opção

--- calc.py
+++ calc.py
@@ -1,28 +1,31 @@
 #calculadora de IMC
 
-#entrada de dados
-peso = input("Digite seu peso: ")
-altura = input("Digite sua altura: ")
+opcao = int(1)
+
+while opcao != 0:
+    #entrada de dados
+    peso = input("Digite seu peso: ")
+    altura = input("Digite sua altura: ")
 
 
-#processamento
-imc = float(peso) / float(altura) ** 2
+    #processamento
+    imc = float(peso) / float(altura) ** 2
 
-#saída
-print("Seu IMC é: ", imc)
+    #saída
+    print("Seu IMC é: ", imc)
 
-#condicionais
-if imc < 18.5:
-    print("Abaixo do peso")
-elif imc >= 18.5 and imc < 25:
-    print("Peso normal")
-elif imc >= 25 and imc < 30:
-    print("Sobrepeso")
-elif imc >= 30 and imc < 35:
-    print("Obesidade grau 1")
-elif imc >= 35 and imc < 40:
-    print("Obesidade grau 2")
-else:
-    print("Obesidade grau 3")
-    
+    #condicionais
+    if imc < 18.5:
+        print("Abaixo do peso")
+    elif imc >= 18.5 and imc < 25:
+        print("Peso normal")
+    elif imc >= 25 and imc < 30:
+        print("Sobrepeso")
+    elif imc >= 30 and imc < 35:
+        print("Obesidade grau 1")
+    elif imc >= 35 and imc < 40:
+        print("Obesidade grau 2")
+    else:
+        print("Obesidade grau 3")
 
+    opcao = int(input("Deseja continuar? 1 - Sim / 0 - Não: "))
